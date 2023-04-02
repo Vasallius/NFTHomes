ead004f0bf6ff892da697630c0709ee8b8ca2de9
Arian Rey <39580184+SwissNuff@users.noreply.github.com>
2023-04-02 09:12:24 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,17 @@
 import Head from "next/head";
 import Navbar from "../components/Navbar";
 import Properties from "./Properties";
+import Listings from "./Listings";
 
 export default function Home() {
   return (
     <>
-      <Navbar />
       <Head>
         <title>Xircus | with Components</title>
         <meta name="description" content="Template created by Xircus" />
         <link rel="icon" href="/logo.png" />
       </Head>
+      <Listings />
       <Properties />
     </>
   );
