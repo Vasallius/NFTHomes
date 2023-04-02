6ad5d2fa4ce13d9d5a08e284d63e9c1b35445496
Arian Rey <39580184+SwissNuff@users.noreply.github.com>
2023-04-02 13:58:20 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Navbar from "../components/Navbar";
 import Properties from "./Properties";
 import Landing from "./Landing";
+import Search from "../components/Search";
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
       </Head>
       <Navbar />
       <Landing />
+      <Search />
       <Properties />
     </>
   );
