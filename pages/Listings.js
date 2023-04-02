51bec6453570532ff431599391fc33103bb0a2d6
Arian Rey <39580184+SwissNuff@users.noreply.github.com>
2023-04-02 09:12:14 +0800

    Refactor: Listings

---
+++ pages/Listings.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import FindAPlace from "./FindAPlace";
+
+const Listings = () => {
+  return (
+    <div>
+      <Navbar />
+      <FindAPlace />
+    </div>
+  );
+};
+
+export default Listings;
