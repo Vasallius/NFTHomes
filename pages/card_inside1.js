7111a96bea8e8ec301415e2ce8889552e443201d
Dox-Dev <josecaluag6@gmail.com>
2023-04-02 12:48:53 +0800

    chore(`card_inside1`): remove extra space in calendar

--- pages/card_inside1.js
+++ pages/card_inside1.js
@@ -118,7 +118,7 @@ export default function Home() {
 
             <Box>
               <Grid
-                h='1100px'
+                h='500px'
                 w='1200px'
                 templateRows='repeat(1, 1fr)'
                 templateColumns='repeat(1, 1fr)'
@@ -126,10 +126,10 @@ export default function Home() {
                 color='blackAlpha.700'
               >
 
-                <GridItem colSpan={10} bg='papayawhip' >
-                  <div class="flex flex-col items-center justify-center min-h-screen px-6 bg-gray-900">
-                    <h1 class="text-2xl font-bold text-center text-white">Calender</h1>
-                    <p class="flex items-end mt-2 text-base text-center text-gray-400 gap-x-2">Built using HTML, Tailwind CSS, and JavaScript. Like and Share for more awesome content
+                <GridItem colSpan={10}>
+                  <div class="flex flex-col items-center justify-center px-6">
+                    <h1 class="text-2xl font-bold text-center text-white">Calendar</h1>
+                    <p class="flex items-end mt-2 text-base text-center text-gray-400 gap-x-2">Find available 
                       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5 text-red-500 shrink-0">
                         <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
                       </svg>
