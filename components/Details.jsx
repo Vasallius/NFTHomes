3b35e12b5ceb1403493dc6d7021487c38f39e1fe
Jedidiah Carl O. Tan <masterjed7262@gmail.com>
2023-04-02 12:44:55 +0800

    added ugly details

---
+++ components/Details.jsx
@@ -0,0 +1,45 @@
+import { Box, Container, Grid, GridItem, Text } from "@chakra-ui/react";
+
+export default function Details() {
+  return (
+    <Box bg="gray.900" color="white">
+      <Container maxW="container.xl" p="12">
+        <Grid templateColumns="repeat(6, 1fr)" gap={6}>
+          <GridItem colSpan={3}>
+            <Text fontSize="xl" fontWeight="bold" mb="4">
+              About this space
+            </Text>
+            <ul>
+              <li>- Relax and unwind in peaceful surroundings</li>
+              <li>- Breathtaking penthouse view of Manhattan skyline</li>
+              <li>- First-class pool amenities</li>
+              <li>- Fully furnished with well-designed interiors</li>
+              <li>- 5 minutes away from the Mall of America and Manhattan Yacht Club</li>
+              <li>- Across from the Cultural Center of the Philippines complex</li>
+            </ul>
+          </GridItem>
+          <GridItem colSpan={3}>
+            <Text fontSize="xl" fontWeight="bold" mb="4">
+              The space
+            </Text>
+            <Text mb="4">
+              Well-designed suite unit complete with new furnishings, Netflix, cable, and wifi for your convenience.
+            </Text>
+            <Text fontSize="lg" fontWeight="bold" mb="4">
+              Guest access
+            </Text>
+            <ul>
+              <li>Guests can access the grand lobby and the two side outdoor pools, along with tables and playgrounds for kids.</li>
+            </ul>
+            <Text fontSize="lg" fontWeight="bold" mb="4">
+              Other things to note
+            </Text>
+            <Text>
+              This unit is newly renovated and designed as a VIP suite for local and foreign staycations, offering an extraordinary view of the Manhattan skyline from the balcony.
+            </Text>
+          </GridItem>
+        </Grid>
+      </Container>
+    </Box>
+  );
+}
--- pages/card_inside1.js
+++ pages/card_inside1.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 //import { Image } from "@chakra-ui/core";
 import Navbar from '../components/Navbar'
+import Details from '../components/Details'
 
 import { Box, Center, Heading, Image, Text, Flex, Container, Button } from "@chakra-ui/react";
 import { ChangeNetwork, ConnectModal, DisconnectButton, Wallet } from "@xircus-web3/components"
@@ -64,7 +65,7 @@ export default function Home() {
                   gap={4}
                   color='blackAlpha.700'
                 >
-                  <GridItem colSpan={3} bg='tomato' > Details </GridItem>
+                  <GridItem colSpan={3}> <Details /> </GridItem>
 
                   <GridItem colSpan={3} bg="gray.800" color="white" borderRadius="lg" boxShadow="lg" p="6">
                     <Text fontSize="2xl" fontWeight="semibold" mb="2">
@@ -127,7 +128,7 @@ export default function Home() {
 
                 <GridItem colSpan={10} bg='papayawhip' >
                   <div class="flex flex-col items-center justify-center min-h-screen px-6 bg-gray-900">
-                    <h1 class="text-2xl font-bold text-center text-white">Tailwind CSS Calender with Hover Effect</h1>
+                    <h1 class="text-2xl font-bold text-center text-white">Calender</h1>
                     <p class="flex items-end mt-2 text-base text-center text-gray-400 gap-x-2">Built using HTML, Tailwind CSS, and JavaScript. Like and Share for more awesome content
                       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5 text-red-500 shrink-0">
                         <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
